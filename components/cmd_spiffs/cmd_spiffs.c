303964699d48ef2a7f791a86069fe9980dc4cf06
Caden Cardell <ccardell@sonicu.com>
2023-03-27 22:27:16 -0400

    adds spiffs

--- components/cmd_spiffs/cmd_spiffs.c
+++ components/cmd_spiffs/cmd_spiffs.c
@@ -19,7 +19,10 @@
 #include "argtable3/argtable3.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
+#include "esp_spiffs.h"
 #include "cmd_spiffs.h"
+
+static const char* TAG = "cmd_spiffs";
 
 /** Arguments used by 'join' function */
 static struct {
@@ -68,6 +71,32 @@ static int cat_file(int argc, char **argv)
 
 void register_spiffs(void)
 {
+
+    ESP_LOGI(TAG, "Initializing SPIFFS");
+
+    esp_vfs_spiffs_conf_t conf = {
+      .base_path = "/spiffs",
+      .partition_label = NULL,
+      .max_files = 5,
+      .format_if_mount_failed = false
+    };
+
+    // Use settings defined above to initialize and mount SPIFFS filesystem.
+    // Note: esp_vfs_spiffs_register is an all-in-one convenience function.
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+
+    if (ret != ESP_OK) {
+        if (ret == ESP_FAIL) {
+            ESP_LOGE(TAG, "Failed to mount or format filesystem");
+        } else if (ret == ESP_ERR_NOT_FOUND) {
+            ESP_LOGE(TAG, "Failed to find SPIFFS partition");
+        } else {
+            ESP_LOGE(TAG, "Failed to initialize SPIFFS (%s)", esp_err_to_name(ret));
+        }
+        return;
+    }
+
+    // register command line things
     list_args.end = arg_end(0);
 
     const esp_console_cmd_t list_cmd = {
